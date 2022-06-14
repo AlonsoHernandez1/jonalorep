69798bf86086f4482aa02d1ef27e99eef9416c70
Jonathon Bell <jonathon.bell@cambridgesemantics.com>
2022-06-14 09:32:38 -0400

    update main

--- q2/src/main.cpp
+++ q2/src/main.cpp
@@ -1,7 +1,26 @@
+//********* Copyright © Alonso Hernandez & Jonathon Bell. All rights reserved.
+//*
+//*
+//*  Version : $Header:$
+//*
+//*
+//*  Purpose :
+//*
+//*
+//*  Comments: This file uses a tab size of 2 spaces.
+//*
+//*
+//****************************************************************************
+
 #include <iostream>
+
+//****************************************************************************
 
 int main(int, char*[])
 {
-    std::cout<<"hello"<<std::endl;
-    return 0;
+  std::cout << "hello alonso " << std::endl;
+
+  return 0;
 }
+
+//****************************************************************************
