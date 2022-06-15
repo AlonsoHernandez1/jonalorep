a449bd388da530d7d7240d44a0a7f816fa696088
Jonathon Bell <jonathon.bell@cambridgesemantics.com>
2022-06-15 08:49:41 -0400

    .

--- q2/src/main.cpp
+++ q2/src/main.cpp
@@ -14,30 +14,41 @@
 
 #include <iostream>
 #include <cstring>
+
 //****************************************************************************
 
 int main(int argc, const char* argv[])
 {
-  if(argc < 2)
+  if (argc < 2)
   {
-    std::cout<<"usage"<<std::endl;
-    return 1;
-  }    
-  for (int i=1;i!=argc;++i )
+    std::cout << "usage: "
+              << argv[0] << "[options] <data-dir> <stopwords.txt>\n"
+              << "options:\n"
+              << "\t--index \n"
+              << "\t--query \n"
+              << std::endl;
+    return 0;
+  }
+
+  for (auto i = 1; i != argc; ++i)
   {
-    if (strcmp(argv[i], "--index") == 0 )
+    if (strcmp(argv[i], "--index") == 0)
     {
         //set index mode
         continue;
     }
-    if (strcmp(argv[i], "--query") == 0 )
+
+    if (strcmp(argv[i], "--query") == 0)
     {
         //set query mode
         continue;
     }
-    std::cout << "bad argument: wanted one of {--index, --query} but got  " <<argv[i]<< std::endl;
+
+    std::cout << "bad option: wanted one of {--index, --query} but got '"
+              << argv[i] << "'" << std::endl;
     return 1;
   }
+
   std::cout << "hello alonso " << std::endl;
 
   return 0;
